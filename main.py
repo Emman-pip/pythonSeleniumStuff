0e8f0f63822692ce676ff91fe30872625ad75907
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-01-28 09:15:12 +0800

    Add basic selenium stuff.

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://python.org")
+
+print(driver.title)
+
+driver.close()
